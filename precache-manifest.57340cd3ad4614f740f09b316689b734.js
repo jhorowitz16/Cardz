ad3d073c39c0296d4a8041fc8ab843255774ad35
Joshua Horowitz <joshua.horowitz@berkeley.edu>
2019-01-22 08:09:39 -0800

    Updates

---
+++ precache-manifest.57340cd3ad4614f740f09b316689b734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Cardz/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9313159902df40ca7cb",
+    "url": "/Cardz/static/js/runtime~main.f9313159.js"
+  },
+  {
+    "revision": "949cc05e2da534394f09",
+    "url": "/Cardz/static/js/main.949cc05e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Cardz/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "949cc05e2da534394f09",
+    "url": "/Cardz/static/css/main.2a0ae2da.chunk.css"
+  },
+  {
+    "revision": "fc3af655426f37da7f9b090593a8deef",
+    "url": "/Cardz/index.html"
+  }
+];
