189ca07d9601b05f97434dd16d1239b103f992cd
Joshua Horowitz <joshua.horowitz@berkeley.edu>
2019-02-18 04:36:46 -0800

    use query params instead due to gh-pages not resolving server urls

--- pokerz/src/App.js
+++ pokerz/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 import {importAll} from './utils.js';
 import Deck from './Deck';
 import {parseWinnerObj, scoreHand, resolveGame} from './utils.js';
-import {BrowserRouter, Route} from 'react-router-dom';
-
+import queryString from 'query-string';
 
 
 class App extends Component {
@@ -26,7 +25,9 @@ class App extends Component {
   }
 
   buildState() {
-    const deck = new Deck(1600);
+    const query = queryString.parse(window.location.search)
+    const seed = ('seed' in query) ? query.seed : 0;
+    const deck = new Deck(parseInt(seed));
     const community = [deck.drawCard(), deck.drawCard(), deck.drawCard(), deck.drawCard(), deck.drawCard()];
     const handOne = [deck.drawCard(), deck.drawCard()];
     const handTwo = [deck.drawCard(), deck.drawCard()];
@@ -37,6 +38,7 @@ class App extends Component {
       deck: deck,
       handOne: handOne,
       handTwo: handTwo,
+      query: query,
       winners: winners,
     };
   }
@@ -60,57 +62,79 @@ class App extends Component {
     });
   }
 
+  renderCommunity() {
+    if (this.state.query.p === "1" || this.state.query.p === "2") {
+      return null;
+    }
+    const onHandOneClick = this.scoreOne.bind(this);
+    const onResolveGame = this.resolveGame.bind(this);
+    const onHandTwoClick = this.scoreTwo.bind(this);
+    return (
+      <div className="App">
+        test new
+        <header className="App-header">
+          <Hand
+            cards={this.state.handOne}
+            scoreHand={onHandOneClick}
+            winners={this.state.winners}
+          />
+          <Hand
+            cards={this.state.community}
+            hasScoreButton
+            isCommunity
+            scoreHand={onResolveGame}
+            winners={this.state.winners}
+          />
+          <Hand
+            cards={this.state.handTwo}
+            scoreHand={onHandTwoClick}
+            winners={this.state.winners}
+          />
+        </header>
+      </div>
+    );
+  }
+
+  renderPlayerOne() {
+    if (this.state.query.p !== "1") {
+      return null;
+    }
+    const onHandOneClick = this.scoreOne.bind(this);
+    return (
+      <div> hand one
+        <Hand
+          cards={this.state.handOne}
+          scoreHand={onHandOneClick}
+          winners={this.state.winners}
+        />
+      </div>
+    );
+  }
+
+  renderPlayerTwo() {
+    if (this.state.query.p !== "2") {
+      return null;
+    }
+    const onHandTwoClick = this.scoreTwo.bind(this);
+    return (
+      <div> hand two
+        <Hand
+          cards={this.state.handTwo}
+          scoreHand={onHandTwoClick}
+          winners={this.state.winners}
+        />
+      </div>
+    );
+  }
+
   render() {
     window.AppState = {};
-    const onHandOneClick = this.scoreOne.bind(this);
-    const onHandTwoClick = this.scoreTwo.bind(this);
-    const onResolveGame = this.resolveGame.bind(this);
     return (
-      <BrowserRouter>
-        <div>
-          <Route exact={true} path='/Cardz/' render={() => (
-            <div className="App">
-              <header className="App-header">
-                <Hand
-                  cards={this.state.handOne}
-                  scoreHand={onHandOneClick}
-                  winners={this.state.winners}
-                />
-                <Hand
-                  cards={this.state.community}
-                  hasScoreButton
-                  isCommunity
-                  scoreHand={onResolveGame}
-                  winners={this.state.winners}
-                />
-                <Hand
-                  cards={this.state.handTwo}
-                  scoreHand={onHandTwoClick}
-                  winners={this.state.winners}
-                />
-              </header>
-            </div>
-          )}/>
-          <Route exact={true} path='/Cardz/1' render={() => (
-            <div> hand one
-              <Hand
-                cards={this.state.handOne}
-                scoreHand={onHandOneClick}
-                winners={this.state.winners}
-              />
-            </div>
-          )}/>
-          <Route exact={true} path='/Cardz/2' render={() => (
-            <div> hand two
-              <Hand
-                cards={this.state.handTwo}
-                scoreHand={onHandTwoClick}
-                winners={this.state.winners}
-              />
-            </div>
-          )}/>
-        </div>
-      </BrowserRouter>
+      <div>
+        { this.renderPlayerOne() }
+        { this.renderPlayerTwo() }
+        { this.renderCommunity() }
+      </div>
     );
   }
 }
